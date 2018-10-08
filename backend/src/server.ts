decb2c9a322b0a64e92f5c3ea9eb7bbe69db3b9c
Augusto Chmieleski <augustocezarchmieleski@gmail.com>
2018-10-08 14:54:12 -0400

    feat(augusto) added db connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,10 +3,19 @@ import * as express from 'express';
 import { json, urlencoded } from 'body-parser';
 import * as helmet from 'helmet';
 import { AddressInfo } from 'net';
+import * as mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
 
 import configureCORS from './lib/configureCors';
 import normalizePort from './lib/normalizePort';
 import router from './routes';
+import getConnectionString from './lib/getConnectionString';
+
+//Initialize envs
+dotenv.config();
+
+//Connect to DB
+mongoose.connect(getConnectionString());
 
 const app = express();
 const server = http.createServer(app);
