f5611539ed6f33e6d7462c1d70b1311fbf424d9e
Augusto Chmieleski <augustocezarchmieleski@gmail.com>
2018-10-08 13:49:48 -0400

    feat(augusto) started final server

---
+++ backend/src/lib/configureCORS.ts
@@ -0,0 +1,25 @@
+import { Express } from 'express';
+
+/**
+ * Added headers to allow API requests
+ *
+ * @param {Express} app The Express app to be configured
+ */
+function configureCORS(app: Express): void {
+  app.all('/api/*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With',
+    );
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+
+    if (req.method.toLowerCase() !== 'options') {
+      return next();
+    }
+
+    return res.send(204);
+  });
+}
+
+export default configureCORS;
